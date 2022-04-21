9ff747d2a681a002a77033516e8c2f3a13fb226b
Igor Krivoy <javadevik@gmail.com>
2022-04-21 12:16:17 +0300

    add PeopleService

---
+++ PeopleService.java
@@ -0,0 +1,19 @@
+class PeopleService {
+	private final List<Person> people;
+	
+	public PeopleService(@NotNull List<Person> people) {
+		this.people = people;
+	}
+	
+	public List<Person> findAll() {
+		return people;
+	}
+	
+	public Person findById(int id) {
+		return people
+			.stream()
+			.filter(p -> p.getId == id)
+			.findAny()
+			.orElseThrow(RuntimeException::new);
+	}
+}
